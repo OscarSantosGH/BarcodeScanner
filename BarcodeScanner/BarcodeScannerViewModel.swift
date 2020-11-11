2f90b93860304db61fa20b010bdc8b2c2b63d616
Oscar Santos <oscarsantos.go@gmail.com>
2020-11-11 18:48:22 -0500

    mod: barcode status logic moved to BarcodeScannerViewModel

--- BarcodeScanner/BarcodeScannerViewModel.swift
+++ BarcodeScanner/BarcodeScannerViewModel.swift
@@ -10,4 +10,12 @@ import SwiftUI
 final class BarcodeScannerViewModel: ObservableObject {
     @Published var scannedCode = ""
     @Published var alertItem: AlertItem?
+    
+    var statusText: String{
+        scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusColor: Color{
+        scannedCode.isEmpty ? .red : .green
+    }
 }
